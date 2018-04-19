5a0381e6459bc2b18dd185dceb7414b8c3131d6c
Dirk Weil <dirk.weil@gedoplan.de>
2018-04-19 14:34:48 +0200

    Lösung zum Java-EE-Quiz Viel zu viele DB-Zugriffe

--- src/main/java/de/gedoplan/quiz/presentation/OrderPresenter.java
+++ src/main/java/de/gedoplan/quiz/presentation/OrderPresenter.java
@@ -5,6 +5,7 @@ import de.gedoplan.quiz.persistence.OrderRepository;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -20,10 +21,31 @@ public class OrderPresenter {
   @Inject
   Log log;
 
+  /*
+   * Lösung zum Java-EE-Quiz "Viel zu viele DB-Zugriffe":
+   *
+   * Statt wie zunächst programmiert und unten noch im Kommentar zu sehen,
+   * bei jedem Aufruf von getOrders zur DB zu gehen, muss eine Statusvariable
+   * - hier orders - eingeführt werden und diese einmalig gefüllt werden.
+   * Dazu dient hier die @PostConstruct-Methode.
+   *
+   * Der Getter wird weiterhin ggf. vielfach aufgerufen, was nun aber harmlos ist.
+   */
+  List<Order> orders;
+
+  @PostConstruct
+  void loadOrders() {
+    this.log.info("loadOrders");
+
+    this.orders = this.orderRepository.findAll();
+  }
+
   public List<Order> getOrders() {
     this.log.info("getOrders");
 
-    return this.orderRepository.findAll();
+    // Alte, falsche Vorgehensweise: Laden der Orders bei jedem Getter-Aufruf
+    // return this.orderRepository.findAll();
+    return this.orders;
   }
 
 }
